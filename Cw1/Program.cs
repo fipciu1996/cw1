b0d29f6f6f929ecc5158e8d9ff785b035aa57b41
Mateusz Filipek <s15348@pjwstk.edu.pl>
2020-03-01 16:24:23 +0100

    Initial commit of project of simple dotNet crawler

---
+++ Cw1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cw1
+{
+    class Program
+    {
+        
+
+        public static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+            HttpResponseMessage response = await httpClient.GetAsync(args[0]);
+            if (response.IsSuccessStatusCode)
+            {
+                var htmlContent = await response.Content.ReadAsStringAsync();
+                Regex regex = new Regex("[a-z]+[a-z0-9]*@[a-z0-9]+\\.[a-z]+\\.[a-z]+", RegexOptions.IgnoreCase);
+                MatchCollection matches = regex.Matches(htmlContent);
+                foreach (var match in matches)
+                {
+                    string path = @"emails.txt";
+                    // This text is added only once to the file.
+                    if (!File.Exists(path))
+                    {
+                        // Create a file to write to.
+                        using (StreamWriter sw = File.CreateText(path))
+                        {
+                            sw.WriteLine(match.ToString());
+                        }
+                    }
+
+                    // This text is always added, making the file longer over time
+                    // if it is not deleted.
+                    using (StreamWriter sw = File.AppendText(path))
+                    {
+                        sw.WriteLine(match.ToString());
+                    }
+
+                    // Open the file to read from.
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        string s = "";
+                        while ((s = sr.ReadLine()) != null)
+                        {
+                            Console.WriteLine(s);
+                        }
+                    }
+                } 
+            } else
+            {
+                Console.WriteLine($"{response}");
+                Console.WriteLine("Something went wrong");
+            }
+        }
+    }
+}
